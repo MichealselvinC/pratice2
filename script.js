7a60161766f09225444967107d77b6a68f0ca0fc
vcmselvin908-svg <vcmselvin908@gmail.com>
2026-07-27 18:30:33 +0530

    new change

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("btn");
+
+button.addEventListener("click", function () {
+    alert("Welcome to the Website!");
+});
+
+function contact() {
+    alert("Thanks for contacting us.");
+}
